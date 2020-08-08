35ad299aca7091834bb0ec3827f765e665c553c1
博瀚君 <bohanjun@qq.com>
2020-08-08 16:10:07 +0800

    Add file 'blog/urls.py', change files 'blog/views.py', 'blog/models.py'
    - Add file 'blog/urls.py'
    - Change file 'blog/views.py'
    - Change file 'blog/models.py'

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+	class Meta:
+		#verbose_name_plural="Categories"
+		verbose_name='分类'
+		verbose_name_plural='分类'
+
+	name=models.CharField(max_length=100)
+	
+	def __str__(self):
+		return self.name
+
+class Tag(models.Model):
+	class Meta:
+		verbose_name='标签'
+		verbose_name_plural='标签'
+
+	name=models.CharField(max_length=100)
+	
+	def __str__(self):
+		return self.name
+
+class Menu(models.Model):
+	class Meta:
+		verbose_name='菜单'
+		verbose_name_plural='菜单'
+
+	name=models.CharField(max_length=100)
+	link=models.CharField(max_length=100,default='')
+	order=models.PositiveSmallIntegerField()
+	
+	def __str__(self):
+		return self.name
+
+class Button(models.Model):
+	class Meta:
+		verbose_name='按钮'
+		verbose_name_plural='按钮'
+
+	name=models.CharField(max_length=100)
+	url=models.URLField()
+	order=models.PositiveSmallIntegerField()
+	
+	def __str__(self):
+		return self.name
+
+class Article(models.Model):
+	class Meta:
+		verbose_name='文章'
+		verbose_name_plural='文章'
+	
+	title=models.CharField(verbose_name='标题',max_length=80,default='无标题')
+	subtitle=models.CharField(verbose_name='副标题',max_length=80,default='')
+	publish_date=models.DateTimeField(verbose_name='发布日期',default=timezone.now)#verbose_name='date published'
+	url_mark=models.CharField(verbose_name='URL 标识',max_length=60)
+	original_article_link=models.CharField(verbose_name='原文链接',max_length=100,default='')
+	content=models.TextField(verbose_name='内容',default='')
+	author=models.ForeignKey(User,verbose_name='作者',on_delete=models.CASCADE,default=0)
+	category=models.ForeignKey(Category,verbose_name='分类',on_delete=models.CASCADE,default=0)
+	tag=models.ManyToManyField(Tag,verbose_name='标签',blank=True,default=0)
+	
+	def __str__(self):
+		return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Article,Menu,Button
 
-# Create your views here.
+
+def index(request):
+	article_list=Article.objects.all().order_by('-publish_date')
+	list_inline_item=Button.objects.all().order_by('order')
+	navigation_menu_list=Menu.objects.all().order_by('order')
+	return render(request,'blog/index.html',{'article_list':article_list,'list_inline_item':list_inline_item,'menu_list':navigation_menu_list})
+
+def article_detail(request,url_mark):
+	article=get_object_or_404(Article,link=url_mark)
+	return render(request,'blog/article.html',{'article':article})
